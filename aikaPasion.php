df4210b522b95accb53ae247b3816084326588cb
jihyelee619 <46389786+jihyelee619@users.noreply.github.com>
2019-01-08 09:10:06 +0900

    delete

--- aikaPasion.php
+++
@@ -1,8 +0,0 @@
-<?php
-//$myData = $_POST["mydata"];
-//$response = array ('my'=> $myData,'res'=> $gameData);
-$response = '{"RESULT" : "0","BONUS": "0","ITEMS": ["0","0","3","2","0","0","0","2","3","0"],"REMAINTICKET" : "10","USETICKET" : "6"}';
-
-echo json_encode($response);
-
-?>
